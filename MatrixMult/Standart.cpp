6716d70e13cda85679a7a629b99b7aa831cfc7c9
HappyRa1n <HappyRa1n@yandex.ru>
2022-11-12 16:16:34 +0500

    Добавлены тесты

---
+++ MatrixMult/Standart.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+#include <random>
+#include "Standart.h"
+using namespace std;
+
+void show_matrix(vector <vector <float>> matrix) {
+	for (int i = 0; i < matrix.size(); i++) {
+		for (int j = 0; j < matrix[i].size(); j++) {
+			cout << matrix[i][j] << " ";
+		}
+		cout << "\n";
+	}
+}
+
+vector <vector <double>> mul_matrix(vector <vector <double>> first_matrix, vector <vector <double>> second_matrix) {
+	int n = first_matrix.size();
+	vector <vector <float>> result_matrix;
+	for (int i = 0; i < n; i++) {
+		result_matrix.push_back(vector <float>());
+		for (int j = 0; j < n; j++) {
+			float sum = 0;
+			for (int k = 0; k < n; k++) {
+				sum += first_matrix[i][k] * second_matrix[k][j];
+			}
+			result_matrix[i].push_back(sum);
+		}
+	}
+	return result_matrix;
+}
---
+++ MatrixMult/Standart.h
@@ -0,0 +1,2 @@
+#pragma once
+vector <vector <double>> mul_matrix(vector <vector <double>> first_matrix, vector <vector <double>> second_matrix);
